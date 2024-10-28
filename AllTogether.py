b974cfdf982dc163637e80f3666151df0dccb010
eb582 <eb582@kent.ac.uk>
2024-10-28 09:34:05 +0000

    comit

--- AllTogether.py
+++ AllTogether.py
@@ -1,6 +1,6 @@
 ### Part Four -- your code goes here. 
 import random                                                  ### inport random library
-variables = [random.randint(1,100) for None in range(10)]      ### generates 10 random number variable's
+variables = [random.randint(1,100) for _ in range(10)]      ### generates 10 random number variable's
 checker = 0                                                    ### starts at 1st generated variable
 while checker < len(variables):                                ### while there are more variables in the list then the checking variable loop
  if variables[checker] % 2 == 0:                               ### if variable divisable by 2 (even)
